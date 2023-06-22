2a38ee767b7d387b605b52296edea6cd80d2ec79
esammohamed <esammohamed17121996@gmail.com>
2023-06-22 11:43:48 +0300

    Meditation Ui

---
+++ app/src/main/java/com/coderboot/simplecomposeexample/BottomMenuContent.kt
@@ -0,0 +1,9 @@
+package com.coderboot.simplecomposeexample
+
+import androidx.annotation.DrawableRes
+
+data class BottomMenuContent(
+    val title: String,
+    @DrawableRes val iconId: Int,
+
+    )
